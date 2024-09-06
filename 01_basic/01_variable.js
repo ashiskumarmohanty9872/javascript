4715e138a4c889aaa33138d9e6e0d8701156c3a7
ashiskumarmohanty9872 <144931369+ashiskumarmohanty9872@users.noreply.github.com>
2024-09-06 20:32:27 +0530

    commit datatypes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail ="ak@123"
+var accountPassword ="123"
+accountCity="odisha"
+let accountState;
+
+//  accountId=2
+/* prefer not to use var bcz of issue in block scope and
+ fuctional scope */
+ 
+ accountEmail="fgh@456"
+ accountPassword="223372278"
+ accountCity="andhra"
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity ,accountState]);
+ 
+
+ 
