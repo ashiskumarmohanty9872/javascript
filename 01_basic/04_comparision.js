49c692041b460de709fd794329d699b96d004879
ashiskumarmohanty9872 <144931369+ashiskumarmohanty9872@users.noreply.github.com>
2024-09-07 13:08:59 +0530

    commit the 04_comparision file

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null >=0); 
+// the reason is that an equality operator(==) and comparision  > < >= work differently. 
+// comparision converts null to a number, treating oity it  as zero
+// console.log(undefined== 0);
+// console.log(undefined> 0);
+// console.log(undefined>= 0);
+
+
+// === (check datatype )
+console.log("2" ===2 );
+
+
